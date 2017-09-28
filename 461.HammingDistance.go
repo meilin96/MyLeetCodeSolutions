12778d01d5f787fe7b7a4b6bd2e6fa32992a8ff6
meilin996 <lin.mei360@gmail.com>
2017-09-28 13:00:39 +0800

    change dir

--- 461.HammingDistance.go
+++
@@ -1,9 +0,0 @@
-func hammingDistance(x int, y int) int {
-    resNum := x^y
-    var res int = 0
-    for resNum != 0{
-        resNum = resNum & (resNum - 1)
-        res++
-    }
-    return res
-}
