b933cfa5b4c9b05ffb311371ec768e84222b916d
meilin996 <lin.mei360@gmail.com>
2017-11-01 20:59:37 +0800

    shell script

--- Solutions/formatAndPullAndCommit.sh
+++ Solutions/formatAndPullAndCommit.sh
@@ -1,6 +1,12 @@
-clang-format-4.0 tem > $1
+if [ ! -n "$1" ] ;then
+    echo "you have not input the file name!"
+    exit
+else
+    echo "the file name you've input is ______$1______"
+fi
+clang-format-3.9 tem > $1
 rm tem
-git pull origin master
+sudo git pull origin master
 git add $1
-git commit -m "$1"
-git push origin master
+sudo git commit -m "$1"
+sudo git push origin master
