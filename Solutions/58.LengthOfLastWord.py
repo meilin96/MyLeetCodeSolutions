fdc5b5eef50522ddb9c0ad939d9534236835f0ab
meilin996 <lin.mei360@gmail.com>
2017-11-17 22:32:22 +0800

    58.py

--- Solutions/58.LengthOfLastWord.py
+++ Solutions/58.LengthOfLastWord.py
@@ -1,8 +1,9 @@
-class Solution(object)
-    : def lengthOfLastWord(self, s)
-    : ""
-      "
-      : type s : str : rtype : int ""
-                                   "
-                               if not len(s)
-    : return 0 l = s.split() if len(l) > 0 : return len(l[-1]) else : return 0
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        if not len(s):
+            return 0
+        l = s.split()
+        if len(l)>0:
+            return len(l[-1])
+        else:
+            return 0
