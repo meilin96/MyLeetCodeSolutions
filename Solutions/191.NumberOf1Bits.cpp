32e1a369828358386f1f202e589d538bc26940ac
lin.mei360@gmail.com <lin.mei360@gmail.com>
2018-04-07 20:53:03 +0800

    191.NumberOf1Bits.cpp

---
+++ Solutions/191.NumberOf1Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while (n) {
+            if (n & 1 == 1)
+                res++;
+            n = n >> 1;
+        }
+        return res;
+    }
+};
