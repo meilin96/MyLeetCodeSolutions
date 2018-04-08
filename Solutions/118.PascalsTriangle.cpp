9ccdfa2df383a0ad5a151454be420e36ede4569b
lin.mei360@gmail.com <lin.mei360@gmail.com>
2018-04-08 17:45:59 +0800

    118.PascalsTriangle.cpp

--- Solutions/118.PascalsTriangle.cpp
+++ Solutions/118.PascalsTriangle.cpp
@@ -1,19 +1,21 @@
 class Solution {
-public:
-  vector<vector<int>> generate(int numRows) {
-    vector<vector<int>> res;
-    if (numRows == 0)
-      return res;
-    res.push_back(vector<int>(1, 1));
-    for (int i = 1; i < numRows; i++) {
-      vector<int> t(1, 1);
-      for (int j = 1; j < i; j++) {
-        int n = res[i - 1][j - 1] + res[i - 1][j];
-        t.push_back(n);
-      }
-      t.push_back(1);
-      res.push_back(t);
+  public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        if (numRows < 1)
+            return res;
+        vector<int> tem(1, 1);
+        res.push_back(tem);
+        if (numRows == 1)
+            return res;
+
+        for (int i = 1; i < numRows; i++) {
+            vector<int> ele(i + 1, 1);
+            for (int j = 1; j < ele.size() - 1; j++) {
+                ele[j] = res[i - 1][j] + res[i - 1][j - 1];
+            }
+            res.push_back(ele);
+        }
+        return res;
     }
-    return res;
-  }
 };
