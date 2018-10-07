d2244afbb1de94442f5140a732b9a9a9d7a932c1
lin.mei360@gmail.com <lin.mei360@gmail.com>
2018-10-07 16:53:48 +0800

    189.RotateArray.go

---
+++ Solutions/189.RotateArray.go
@@ -0,0 +1,8 @@
+func rotate(nums[] int, k int){k = k % len(nums) reverse(nums, 0, len(nums) - 1)
+                                           reverse(nums, 0, k - 1)
+                                               reverse(nums, k, len(nums) - 1)}
+
+func reverse(data[] int, s, e int) {
+                    for
+                        s < e { data[s], data[e] = data[e], data[s] s++ e-- }
+}
